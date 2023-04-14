17ae50748be4c050d066f78f82100401ccc0b9d3
Mukunta <42895755+Mukunta@users.noreply.github.com>
2023-04-14 04:03:09 +0200

    First Commit For This Project

---
+++ create_users.php
@@ -0,0 +1,33 @@
+<?php
+// Create a connection to the MySQL database
+$conn = new mysqli('localhost', 'root', '', 'inventory');
+
+// Check for errors
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+// Check if the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Sanitize the user input
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password
+    $role = mysqli_real_escape_string($conn, $_POST['role']);
+
+    // Prepare the SQL query
+    $stmt = $conn->prepare("INSERT INTO user (name, email, password, role) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $email, $password, $role);
+
+    // Execute the query and check for errors
+    if ($stmt->execute() === TRUE) {
+        echo "New user created successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the statement and connection
+    $stmt->close();
+    $conn->close();
+}
+?>
